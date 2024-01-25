51b0ab0072fa9d40255edf817c68359d645a0aa2
frarosset <37275581+frarosset@users.noreply.github.com>
2024-01-26 00:26:12 +0100

    Show the status of fulfillment of the password requirements

--- js/script.js
+++ js/script.js
@@ -1,7 +1,54 @@
-pwInput = document.querySelector('.form-control input#password');
-pwConfirmInput = document.querySelector('.form-control input#password-confirm');
+let pwInput = document.querySelector('.form-control input#password');
+let pwConfirmInput = document.querySelector('.form-control input#password-confirm');
+let pwRuleLength = document.querySelector('.pw-rules .pw-length');
+let pwUppercaseLength = document.querySelector('.pw-rules .pw-uppercase');
+let pwLowercaseLength = document.querySelector('.pw-rules .pw-lowercase');
+let pwNumberLength = document.querySelector('.pw-rules .pw-number');
+let pwSpecialLength = document.querySelector('.pw-rules .pw-special');
 
 
+function checkPassword(e){
+    let ruleLengthRegex = /^.{8,30}$/;
+    let ruleUppercaseRegex = /[A-Z]/;
+    let ruleLowercaseRegex = /[a-z]/;
+    let ruleNumberRegex = /[0-9]/;
+    let ruleSpecialRegex = /[\W_]/;
+
+    if (!ruleLengthRegex.test(pwInput.value)){
+        pwRuleLength.classList.add('invalid');
+    }
+    else{
+        pwRuleLength.classList.remove('invalid');
+    }
+
+    if (!ruleUppercaseRegex.test(pwInput.value)){
+        pwUppercaseLength.classList.add('invalid');
+    }
+    else{
+        pwUppercaseLength.classList.remove('invalid');
+    }
+
+    if (!ruleLowercaseRegex.test(pwInput.value)){
+        pwLowercaseLength.classList.add('invalid');
+    }
+    else{
+        pwLowercaseLength.classList.remove('invalid');
+    }
+
+    if (!ruleNumberRegex.test(pwInput.value)){
+        pwNumberLength.classList.add('invalid');
+    }
+    else{
+        pwNumberLength.classList.remove('invalid');
+    }
+
+    if (!ruleSpecialRegex.test(pwInput.value)){
+        pwSpecialLength.classList.add('invalid');
+    }
+    else{
+        pwSpecialLength.classList.remove('invalid');
+    }
+}
 
 function checkPasswordMatch(e){
     if (pwInput.value!==pwConfirmInput.value){
@@ -14,5 +61,6 @@ function checkPasswordMatch(e){
     }
 }
 
+pwInput.addEventListener('input',checkPassword);
 pwInput.addEventListener('input',checkPasswordMatch);
 pwConfirmInput.addEventListener('input',checkPasswordMatch);
